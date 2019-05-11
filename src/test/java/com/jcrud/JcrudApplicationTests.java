465fd9885856566cf22f8989b12832c7c131e049
KarMen <karolmendza@gmail.com>
2019-05-11 22:34:39 +0200

    initial commit

---
+++ src/test/java/com/jcrud/JcrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jcrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JcrudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
